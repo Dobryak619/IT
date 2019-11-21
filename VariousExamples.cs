b1507ae024df3c0e19a9ddeec7e3014dde22b467
Dobryak619 <57769440+Dobryak619@users.noreply.github.com>
2019-11-21 20:02:08 +0100

    Add files via upload

---
+++ VariousExamples.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VariousExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OutParameters();
+            Console.WriteLine(Average());
+
+        }
+        private static double Average(params int [] array)
+        {
+            if (array.Length == 0) return 0;
+
+            double sum = 0;
+            foreach (var item in a)
+            {
+                sum += item;
+            }
+            return sum / array.Length;
+        }
+        private static void OutParameters()
+        {
+            int a = 5;
+            int b = 9;
+            Swap(ref a, ref b);
+            Console.WriteLine($"a is {a}");
+            Console.WriteLine($"b is {b}");
+
+            if (int.TryParse("dieci", out int c))
+            {
+                Console.WriteLine("The number is " + c);
+            }
+            else
+            {
+                Console.WriteLine("This is smth I cannot parse");
+            }
+        }
+
+        private static void MethodWithOut (out int a)
+        private static void Swap (ref int a, ref int b)
+    }
+}
